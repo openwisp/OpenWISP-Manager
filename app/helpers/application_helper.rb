50bde7da0c114e570f823ca9acc074bc5a8c6f57
Matteo Latini <mtylty@gmail.com>
2011-01-25 10:51:13 +0000

    fixes problem with observe_fields (caused by jrails)

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -2,22 +2,10 @@
 module ApplicationHelper
   
   def observe_fields(fields, options)
-    #prepare a value of the :with parameter
-    with = ""
-    for field in fields
-      with += "'"
-      with += "&" if field != fields.first
-      with += field + "='+escape($('" + field + "').value)"
-      with += " + " if field != fields.last
+    ret = ""
+    fields.each do |field|
+      ret += observe_field(field, options.merge({ :with => field }))
     end
-
-    #generate a call of the observer_field helper for each field
-    ret = "";
-    for field in fields
-      ret += observe_field(field,
-      options.merge( { :with => with }))
-    end
-    ret
   end
 
 end
