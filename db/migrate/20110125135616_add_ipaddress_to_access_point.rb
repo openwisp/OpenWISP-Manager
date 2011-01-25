d26979042716ddd67384337baa8f1b880a9dc348
Davide Guerri <davide.guerri@gmail.com>
2011-01-25 16:26:19 +0000

    ranames ipaddress to last_configuration_retrieve_ip

--- db/migrate/20110125135616_add_ipaddress_to_access_point.rb
+++
@@ -1,9 +0,0 @@
-class AddIpaddressToAccessPoint < ActiveRecord::Migration
-  def self.up
-    add_column :access_points, :ip_address, :string
-  end
-
-  def self.down
-    remove_column :access_points, :ip_address
-  end
-end
