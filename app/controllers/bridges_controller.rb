35c030263352227e053a50f2b17a627688f18641
Matteo Latini <mtylty@gmail.com>
2011-02-17 14:33:02 +0000

    removes edit action for bridges_controller (#26), adds last_configuration_retrieve_ip to access points show view

--- app/controllers/bridges_controller.rb
+++ app/controllers/bridges_controller.rb
@@ -3,7 +3,7 @@ class BridgesController < ApplicationController
 
   before_filter :load_wisp
   before_filter :load_access_point
-    
+
   access_control do
     default :deny
 
@@ -11,26 +11,14 @@ class BridgesController < ApplicationController
       allow :wisps_viewer
       allow :access_points_viewer, :of => :wisp
     end
-
-    #TODO: Update actions
-    actions :edit, :update do
-      allow :wisps_manager
-      allow :access_points_manager, :of => :wisp
-    end
   end
 
   # GET /wisps/:wisp_id/access_points/:access_point_id/bridges
   def index
     @bridges = @access_point.bridges.find(:all)
-    
+
     respond_to do |format|
       format.html # index.html.erb
     end
-  end
-
-  # TODO: Write edit action (route and view are defined but
-  # the controller is missing)
-  def edit
-    render :nothing => true
   end
 end
