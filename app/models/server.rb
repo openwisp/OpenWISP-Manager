f59dcd967b5912ece44e7c67aaf0d0de49f5f4a1
Davide Guerri <davide.guerri@gmail.com>
2011-02-25 17:48:14 +0000

    adds minus to the permitted chars list for a server name

--- app/models/server.rb
+++ app/models/server.rb
@@ -1,7 +1,7 @@
 class Server < ActiveRecord::Base
   validates_uniqueness_of :name
   validates_presence_of :name
-  validates_format_of :name, :with => /\A[a-z][\s\w\d\.]*\Z/i
+  validates_format_of :name, :with => /\A[a-z][\s\w\d\.\-]*\Z/i
   validates_length_of :name, :maximum => 16
   
   has_many :ethernets, :as => :machine, :dependent => :destroy
