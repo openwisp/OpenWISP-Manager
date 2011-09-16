c1d19d7eda04feb6b233d0de4e9de1dbe6f104b5
Matteo Latini <mtylty@gmail.com>
2011-09-16 11:39:04 +0000

    adds proper licenses

--- app/models/server.rb
+++ app/models/server.rb
@@ -1,3 +1,20 @@
+# This file is part of the OpenWISP Manager
+#
+# Copyright (C) 2010 CASPUR (wifi@caspur.it)
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
 class Server < ActiveRecord::Base
   validates_uniqueness_of :name
   validates_presence_of :name
