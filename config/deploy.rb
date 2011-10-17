f3be018ad53afd3a0478d4fdaeeb46dde037c4d3
Matteo Latini <mtylty@gmail.com>
2011-10-17 12:46:19 +0000

    ooops, wrong app name :) again!

--- config/deploy.rb
+++ config/deploy.rb
@@ -2,12 +2,12 @@
 # GENERAL SETTINGS
 # =============================================================================
 
-set :application,  "owums"
+set :application,  "owm"
 set :deploy_to,  "/var/rails/#{application}"
 set :rails_env, "production"
 
 set :scm, :subversion
-set :repository, "https://spider.caspur.it/svn/owums/trunk"
+set :repository, "https://spider.caspur.it/svn/owm/trunk"
 
 set :rvm_ruby_string, 'ree'
 
